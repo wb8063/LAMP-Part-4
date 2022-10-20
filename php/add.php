a1d4c8b0d61e428aa58eabe4c9ce1c85a1b7499b
Willow B <wbwasb@gmail.com>
2022-10-20 12:55:00 -0600

    Updated username and password so i can access

--- php/add.php
+++ php/add.php
@@ -23,9 +23,9 @@ echo "<p><strong>$firstname</strong> has been added.</p>";
 // DATABASE OPERATIONS:
 // https://www.w3schools.com/php/php_mysql_insert.asp
 $servername = "localhost";
-$username = "user22";
-$password = "22xxxx";
-$dbname = "db22";
+$username = "user72";
+$password = "72pill";
+$dbname = "db72";
 
 // Create connection (assuming these exist -- we set up the DB on the CLI)
 $conn = new mysqli($servername, $username, $password, $dbname);
